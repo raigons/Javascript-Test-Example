7dd9946e76d4773b7aa00f777442a403a0499a06
Ramon Gonçalves <raigons@gmail.com>
2012-02-15 19:36:57 -0200

    files required for testing

---
+++ module_a_test.js
@@ -0,0 +1,25 @@
+module("Module A");
+test("testing module A inside a js file", function(){
+	ok(true, 'this test is fine =]');
+	var value = 1;
+	var obj = {value: 1}
+	equal(value, obj.value, "We expect value to be '1'");
+});
+
+test("Test an object property", function(){
+	var object = {"properti": "property value"};
+	ok(object.hasOwnProperty("property"), "The object should have an attribute called 'property'");
+});
+
+test("Agora testando em português feliz =]", function(){
+	var Objeto = function(){
+		var valor = 2.0;
+		return {
+			multiplicarPor: function(produto){
+				return valor * produto;
+			}
+		}			 
+	};
+	var obj = Objeto();
+	equal(4, obj.multiplicarPor(2), 'O valor esperado como resultado da multiplicação é 4');
+});
